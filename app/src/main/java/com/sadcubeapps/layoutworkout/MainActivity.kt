d808db92ddbf7a1d447e9fbad5d83c341a1d7726
Vlad Prosekov <vladprosekov2004@gmail.com>
2020-07-28 10:59:50 +0600

    Initial commit

---
+++ app/src/main/java/com/sadcubeapps/layoutworkout/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.sadcubeapps.layoutworkout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+    }
+}
