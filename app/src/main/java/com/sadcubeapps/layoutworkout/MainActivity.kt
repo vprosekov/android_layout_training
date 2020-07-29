9e02df511e14a45df8e8812948baca1828c751e5
Vlad Prosekov <vladprosekov2004@gmail.com>
2020-07-29 14:45:13 +0600

    added databinding, made own styles, strings, adaptive view size

--- app/src/main/java/com/sadcubeapps/layoutworkout/MainActivity.kt
+++ app/src/main/java/com/sadcubeapps/layoutworkout/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.sadcubeapps.layoutworkout
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.sadcubeapps.layoutworkout.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        try {
-            this.supportActionBar!!.hide()
-        } catch (e: NullPointerException) {
-        }
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        activityMainBinding.button.setOnClickListener{  changeText()}
+    }
+
+    private fun changeText() {
+        activityMainBinding.textView.text = "Data Binding"
     }
 }
